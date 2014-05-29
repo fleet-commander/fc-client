7049c2f043b8fdcdf1b2e57f3f9d0043c35d962e
Matthew Barnes <mbarnes@redhat.com>
2014-05-29 10:33:50 -0400

    Fix quoting error in shell scripts.

--- data/fleet-commander.sh
+++ data/fleet-commander.sh
@@ -4,7 +4,7 @@ mkdir -p `dirname $dconf_profile`
 
 # Use --print-reply to wait for a reply message.
 # This ensures the service can lookup the user ID.
-dbus-send --system --print-reply --dest=org.gnome.FleetCommander --type=method_call /org/gnome/FleetCommander org.gnome.FleetCommander.DConf.WriteProfile string:'$dconf_profile' > /dev/null
+dbus-send --system --print-reply --dest=org.gnome.FleetCommander --type=method_call /org/gnome/FleetCommander org.gnome.FleetCommander.DConf.WriteProfile "string:$dconf_profile" > /dev/null
 
 if [ $? -eq 0 ]
   then DCONF_PROFILE=$dconf_profile
