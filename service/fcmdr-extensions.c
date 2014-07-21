8220aef889c388bee08be564edd0804a02d0f5af
Matthew Barnes <mbarnes@redhat.com>
2014-07-21 12:52:08 -0400

    Add FCmdrYumBackend.
    A new service backend that acts on the "org.baseurl.yum" member of a
    profile's settings object.  The JSON member is just the yum repo file
    content as a string, which defines one or more yum repositories.  The
    service backend simply writes the content verbatim to:
    
       /etc/yum.repos.d/fleet-commander-$PROFILE_UID.repo
    
    Repo files are deleted automatically when the corresponding profile
    is no longer published by the server.

--- service/fcmdr-extensions.c
+++ service/fcmdr-extensions.c
@@ -26,6 +26,7 @@
 
 extern GType fcmdr_gsettings_backend_get_type (void);
 extern GType fcmdr_http_profile_source_get_type (void);
+extern GType fcmdr_yum_backend_get_type (void);
 
 /* Helper for fcmdr_ensure_extension_points_registered() */
 static gpointer
@@ -61,4 +62,5 @@ fcmdr_ensure_extensions_registered (void)
 
 	g_type_ensure (fcmdr_gsettings_backend_get_type ());
 	g_type_ensure (fcmdr_http_profile_source_get_type ());
+	g_type_ensure (fcmdr_yum_backend_get_type ());
 }
